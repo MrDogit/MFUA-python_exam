1b28910577f79d34d4eb26d8d80fd89f24d66168
Felix Moro <FelixMoro.MrDogit@gmail.com>
2022-11-27 11:43:01 +0300

    spaces

--- 17.py
+++ 17.py
@@ -8,12 +8,12 @@ x3 = d['x'][2]
 y3 = d['y'][2]
 
 # a - 12 b - 23 c - 31
-a = sqrt((x2-x1)**2+(y2-y1)**2)
-b = sqrt((x3-x2)**2+(y3-y2)**2)
-c = sqrt((x3-x1)**2+(y3-y1)**2)
-if a>b and a>c:
+a = sqrt( ( x2 - x1 ) ** 2 + ( y2 - y1 ) ** 2 )
+b = sqrt( ( x3 - x2 ) ** 2 + ( y3 - y2 ) ** 2 )
+c = sqrt( ( x3 - x1 ) ** 2 + ( y3 - y1 ) ** 2 )
+if a > b and a > c:
     print(a)
-elif b>a and b>c:
+elif b > a and b > c:
     print(b)
 else:
     print(c)
