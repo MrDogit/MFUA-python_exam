9bfc17a5a4ca7d6e3b0273d1436d27c8731429e1
Felix Moro <felixmoro.mrdogit@gmail.com>
2022-12-16 14:12:13 +0300

    numbers (16.12.2022)

---
+++ numbers/1.py
@@ -0,0 +1,53 @@
+import numbers
+import numpy as np
+import random
+
+one =   list('001001001001001')
+two =   list('111001111100111')
+three = list('111001111001111')
+four =  list('101101111001001')
+five =  list('111100111001111')
+six =   list('111100111101111')
+seven = list('111001001001001')
+eight = list('111101111101111')
+nine =  list('111101111001111')
+zero =  list('111101101101111')
+
+numsbers = [one, two, three, four, five, six, seven, eight, nine, zero]
+
+tema = 5
+n_sensor = 15
+
+def perceptron(sensor):
+    b = 7
+    s = 0
+
+    for i in range(n_sensor):
+            pass
+
+def decrement(numbers):
+    for i in range(n_sensor):
+        if int (numbers[i])==1:
+            w-=1
+
+def increment(numbers):
+    for i in range(n_sensor):
+        if int(numbers[i]):
+            w+=1
+
+n = 100000
+for i in range(i):
+    j = random.randint(0.9)
+    j = perceptron(numbers[i])
+    if j != tema:
+        if not r:
+            increase(numbers[tema])
+
+print(w)
+f_one = list
+
+
+
+
+
+
